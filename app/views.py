645ae4ed35faed1c6c151d60830847382143d086
shunzi <shunzi2012@gmail.com>
2014-01-13 23:06:44 +0800

    add login view

--- app/views.py
+++ app/views.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/index', methods = ['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+
+    redirect(url_for('index'))
 
 @app.route('/query_account')
 def query_account():
